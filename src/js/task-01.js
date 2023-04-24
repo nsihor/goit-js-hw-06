ab14d4c7144620fe669a3fca0eb4762deab8d8e0
nsihor <nsihor@gmail.com>
2023-04-25 00:54:54 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,11 +1,11 @@
 const categories = document.querySelector('#categories');
+const categoriesItems = Array.from(categories.children);
 
-const categoriesItems = categories.querySelectorAll('.item')
 console.log(`Number of categories: ${categoriesItems.length}`);
 console.log('');
 
 categoriesItems.forEach(item => {
-    console.log(`Category: ${item.querySelector('h2').textContent}`);
-    console.log(`Elements: ${item.querySelectorAll('li').length}`);
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}`);
     console.log('');
 });
